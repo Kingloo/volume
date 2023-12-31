b690ef8c264fc507c0f5990d0da9ae8ed6473e38
Kingloo <k1ngloo@hotmail.com>
2023-12-31 15:23:57 +0100

    Simplify working out desired new volume, bump programme version

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#![allow(unused_imports)]
-
 use windows::core::Result;
 use windows::Win32::Media::Audio::Endpoints::IAudioEndpointVolume;
 use windows::Win32::Media::Audio::{eConsole, eRender, IMMDevice, IMMDeviceEnumerator, MMDeviceEnumerator};
@@ -13,22 +11,27 @@ fn main() -> Result<()> {
 
 		let audio_endpoint_volume: IAudioEndpointVolume = create_audio_endpoint_volume()?;
 
+		let current_volume: f32 = get_volume(&audio_endpoint_volume)?;
+
 		if args.len() < 2 {
-			print_volume(get_volume(&audio_endpoint_volume)?);
+			println!("current master volume {:.0}%", convert_float_to_percent(current_volume));
 			return Ok(());
 		}
 
-		if let Ok(desired_volume_scalar) = args[1].parse::<f32>() {
-			if desired_volume_scalar > 1.0 {
-				println!("value too large {:?}", desired_volume_scalar);
-				return Ok(());
-			}
-			set_volume(desired_volume_scalar, &audio_endpoint_volume)?;
-		} else if args[1].as_str() == "inc" {
-			increment_volume(&audio_endpoint_volume)?;
-		} else if args[1].as_str() == "dec" {
-			decrement_volume(&audio_endpoint_volume)?;
+		let desired_volume_scalar: f32 = match args[1].as_str() {
+			"inc" => current_volume + 0.01,
+			"dec" => current_volume - 0.01,
+			str => str.parse::<f32>().unwrap_or(current_volume)
+		};
+
+		if !(0.0..=1.0).contains(&desired_volume_scalar) {
+			eprintln!("failed: value must be between 0.0 and 1.0");
+			return Ok(());
 		}
+
+		set_volume(desired_volume_scalar, &audio_endpoint_volume)?;
+
+		println!("set master volume to {:.0}%", convert_float_to_percent(desired_volume_scalar));
 	}
 
 	Ok(())
@@ -48,26 +51,7 @@ unsafe fn get_volume(audio_endpoint_volume: &IAudioEndpointVolume) -> Result<f32
 
 unsafe fn set_volume(desired_volume_scalar: f32, audio_endpoint_volume: &IAudioEndpointVolume) -> Result<()> {
 	audio_endpoint_volume.SetMasterVolumeLevelScalar(desired_volume_scalar, std::ptr::null())?;
-	println!("set master volume to {:.0}%", convert_float_to_percent(desired_volume_scalar));
 	Ok(())
-}
-
-unsafe fn increment_volume(audio_endpoint_volume: &IAudioEndpointVolume) -> Result<f32> {
-	let current_volume: f32 = get_volume(audio_endpoint_volume)?;
-	let new_volume: f32 = current_volume + 0.01;
-	set_volume(new_volume, audio_endpoint_volume)?;
-	Ok(new_volume)
-}
-
-unsafe fn decrement_volume(audio_endpoint_volume: &IAudioEndpointVolume) -> Result<f32> {
-	let current_volume: f32 = get_volume(audio_endpoint_volume)?;
-	let new_volume: f32 = current_volume - 0.01;
-	set_volume(new_volume, audio_endpoint_volume)?;
-	Ok(new_volume)
-}
-
-fn print_volume(volume: f32) {
-	println!("current master volume {:.0}%", convert_float_to_percent(volume));
 }
 
 fn convert_float_to_percent(volume: f32) -> f32 {
